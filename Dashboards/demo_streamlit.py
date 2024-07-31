4143563fa153b58b752947918fa7c43b6fda91e2
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-07-31 06:28:06 +0000

    avancement calcul avec filtre artificialisation

--- Dashboards/demo_streamlit.py
+++ Dashboards/demo_streamlit.py
@@ -11,9 +11,7 @@ import numpy as np
 lichen_ecology = df.get_lichen_ecology()
 lichen_frequency = df.get_lichen_frequency()
 
-st.write(lichen_ecology)
-
-# Calcul
+# Calcul somme des fréquences
 def calc_frequences(df):
     df_agg = df.groupby("main_lichenspecies").agg({
         "id": "first",
@@ -23,15 +21,45 @@ def calc_frequences(df):
     return df_agg
 
 calc_freq = calc_frequences(lichen_frequency)
+calc_freq = calc_freq[["main_lichenspecies", "frequency"]]
+
+def deg_artif(id_site: int, species_name : str):
+    # Calcul filtrable
+    freq = lichen_frequency[lichen_frequency["id"] == id_site]["frequency"].values[0]
+    freq_g = calc_freq[calc_freq["main_lichenspecies"] == species_name]["frequency"].values[0] 
+
+    return round(freq / freq_g * 100, 2)
 
 
-# Calculer les proportions des espèces selon leur tolérance à l'anthropisation
-artificialisation_proportions = lichen_ecology['poleotolerance'].value_counts(normalize=True) * 100
+# Sélection du site 
+id_site = st.selectbox(
+    "Sur quel site voulez-vous ?",
+    lichen_frequency["id"],
+    index=None,
+    placeholder="site n°",
+)
+
+# Sélection des espèces 
+species_name = st.selectbox(
+    "Sur quel espèce voulez-vous ?",
+    calc_freq["main_lichenspecies"],
+    index=None,
+    placeholder="Je sélectionne l'espèce...",
+)
+
+# Affichage des éléments
+if id_site and species_name != None:
+    pass
+else:
+    id_site = 460
+    species_name = "Physcia aipolia/stellaris"
+# le calcul
+artificialisation_proportions = deg_artif(id_site, species_name)
 
 # # Dataviz charts
 fig1 = go.Figure(go.Indicator(
     domain = {'x': [0, 1], 'y': [0, 1]},
-    value = artificialisation_proportions["intermediate"],
+    value = artificialisation_proportions,
     mode = "gauge+number",
     title = {'text': "Degré d'artificialisation"},
     gauge = {'axis': {'range': [0, 100], 'dtick': 25},
@@ -46,7 +74,7 @@ fig1 = go.Figure(go.Indicator(
                  {'range': [50, 75], 'color': "orange"},
                  {'range': [75, 100], 'color': "red"}
                  ],
-             'threshold' : {'line': {'color': "#000000", 'width': 4}, 'thickness': 0.75, 'value': artificialisation_proportions["intermediate"]}
+             'threshold' : {'line': {'color': "#000000", 'width': 4}, 'thickness': 0.75, 'value': artificialisation_proportions}
              }))
 
 x_values = [10, 8, 6, 5, 4, 3, 2, 1]
@@ -81,7 +109,7 @@ hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_v
 st.title("Dataviz POC")
 tab1, tab2, tab3= st.tabs(["Gauge", "Histogram", "df Fréquences"])
 with tab1:
-    st.write("**Mode de calcul**`df['poleotolerance'].value_counts(normalize=True) * 100`")
+    st.write(f"Degrés d'artificialisation sur le site n°**{id_site}** pour l'espèce **{species_name}**")
     st.plotly_chart(fig1)
 
 with tab2:
