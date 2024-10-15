fb8d6060645dfead9266063ba7e2cdd1b9a42368
Benoit Frisque <benoitfrisque@gmail.com>
2024-10-15 11:05:41 +0200

    refined layout and dark mode

--- Dashboards/constants.py
+++ Dashboards/constants.py
@@ -1,21 +1,52 @@
-from utils.css_reader import get_css_properties
+BODY_FONT_FAMILY = '"Source Sans Pro", sans-serif'
+PLOTLY_FONT_COLOR = "#495057" # Grey
 
 # Constants for color palettes, font families, etc.
+# BASE_COLOR_PALETTE = [
+#     "#387CA6",
+#     "#1C6C8C",
+#     "#3887A6",
+#     "#ADCCD9",
+#     "#F2F2F2"
+# ]
+
+
+# Generated with https://omatsuri.app/color-shades-generator
+
 BASE_COLOR_PALETTE = [
+    # "#333D43",
+    # "#37444C",
+    # "#3A4C58",
+    # "#3C5665",
+    # "#3D6176",
+    # "#3C6D8C",
     "#387CA6",
-    "#1C6C8C",
-    "#3887A6",
-    "#ADCCD9",
-    "#F2F2F2"
+    "#4A86AB",
+    "#608FAD",
+    "#799AAF",
+    "#90A7B5",
+    "#A6B6BF",
+    "#BDC6CC",
 ]
 
 PASTEL_COLOR_PALETTE = [
-    '#c3d7e4',
-    '#bad2dc',
-    '#c3dbe4',
-    '#e6eff3',
-    '#fbfbfb'
+    # "#c1c4c6",
+    # "#c3c6c9",
+    # "#c3c9cc",
+    # "#c4ccd0",
+    # "#c4cfd5",
+    # "#c4d3dc",
+    "#c3d7e4",
+    "#c8dae5",
+    "#cfdde6",
+    "#d6e0e7",
+    "#dde4e8",
+    "#e4e9eb",
+    "#ebedef",
 ]
+
+
+SQUARE_COLUMNS = [f'sq{i}' for i in range(1, 6)]
 
 ORIENTATIONS_MAPPING = {
     "N": "Nord",
@@ -26,11 +57,16 @@ ORIENTATIONS_MAPPING = {
 
 ORIENTATIONS = list(ORIENTATIONS_MAPPING.keys())
 
-SQUARE_COLUMNS = ['sq1', 'sq2', 'sq3', 'sq4', 'sq5']
-
-BODY_STYLE = get_css_properties("body")
-BODY_FONT_FAMILY = BODY_STYLE.get("font-family", "Arial")
-BODY_FONT_COLOR = BODY_STYLE.get("color", "grey")
+MAP_SETTINGS = {
+    "nb_species_cat": {
+        "title": "Nombre d'espèces",
+        "color_map": {'<7': 'red', '7-10': 'orange', '11-14': 'yellow', '>14': 'green'}
+    },
+    "VDL_cat": {
+        "title": "VDL",
+        "color_map": {'<5': 'red', '5-10': 'orange', '10-15': 'yellow', '>15': 'green'}
+    }
+}
 
 # Define the plotly style for hover labels
 PLOTLY_HOVER_STYLE = {
@@ -43,10 +79,13 @@ PLOTLY_HOVER_STYLE = {
 PLOTLY_LAYOUT = {
     "font": dict(
         family=BODY_FONT_FAMILY,
-        color=BODY_FONT_COLOR
+        color=PLOTLY_FONT_COLOR
     ),
     "template": "plotly_white",
-    "margin": dict(l=10, r=10, t=10, b=10),
+    "margin": dict(l=0, r=0
+                   , t=10, b=10),
     "barcornerradius":"30%",
-    "hoverlabel": PLOTLY_HOVER_STYLE
+    "hoverlabel": PLOTLY_HOVER_STYLE,
+    "plot_bgcolor": "rgba(0, 0, 0, 0)",  # Transparent plot background
+    "paper_bgcolor": "rgba(0, 0, 0, 0)", # Transparent paper background
 }
