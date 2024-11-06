fcb72dae0ea71a2298e1966feae5c8d54c0137fd
Benoit Frisque <benoitfrisque@gmail.com>
2024-11-06 11:37:56 +0100

    added adress locator, translation for charts and hover data

--- dashboard/charts.py
+++ dashboard/charts.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from dashboard.constants import BASE_COLOR_PALETTE, PASTEL_COLOR_PALETTE, PLOTLY_LAYOUT, MAP_COLOR_PALETTES
@@ -20,22 +19,20 @@ def blank_figure():
 
     return fig
 
-def create_map_observations(filtered_df, map_column_selected, zoom, center, map_style, observation_clicked=None):
-
-    filtered_df['date_obs'] = pd.to_datetime(filtered_df['date_obs']).dt.strftime('%d/%m/%Y')
+def create_map_observations(filtered_df, map_column_selected, zoom, center, map_style, lang, observation_clicked=None):
 
     fig_map = px.scatter_map(
         filtered_df,
         lat='localisation_lat',
         lon='localisation_long',
         color=map_column_selected,
-        hover_name='date_obs',
+        hover_name='date_obs_formatted',
         labels={  # Rename the columns for the legend
-            'nb_species_cat': 'Nombre d\'espèces',
-            'VDL_cat': 'Valeur de Diversité Lichénique',
-            'deg_toxitolerance_cat': 'Espèces toxitolérantes',
-            'deg_eutrophication_cat': 'Espèces eutrophes',
-            'deg_acidity_cat': 'Espèces acidophiles',
+            'nb_species_cat': get_translation('nb_species', lang=lang).capitalize(),
+            'VDL_cat': get_translation('VDL_cat', lang=lang),
+            'deg_toxitolerance_cat': get_translation('deg_toxitolerance_cat', lang=lang),
+            'deg_eutrophication_cat': get_translation('deg_eutrophication_cat', lang=lang),
+            'deg_acidity_cat': get_translation('deg_acidity_cat', lang=lang),
         },
         custom_data=['nb_species', 'VDL', 'deg_toxitolerance', 'deg_eutrophication', 'deg_acidity'],
         map_style=map_style,
@@ -44,14 +41,15 @@ def create_map_observations(filtered_df, map_column_selected, zoom, center, map_
     )
 
     fig_map.update_traces(
-        hovertemplate='<b>Date d\'observation</b>: %{hovertext}<br>'
-                    '<b>Latitude</b>: %{lat}<br>'
-                    '<b>Longitude</b>: %{lon}<br>'
-                    '<b>Nombre d\'espèces</b>: %{customdata[0]}<br>'
-                    '<b>Valeur de Diversité Lichénique</b>: %{customdata[1]:.1f}<br>'
-                    '<b>Espèces toxitolérantes</b>: %{customdata[2]:.1%}<br>'
-                    '<b>Espèces eutrophes</b>: %{customdata[3]:.1%}<br>'
-                    '<b>Espèces acidophiles</b>: %{customdata[4]:.1%}<br>'
+        hovertemplate= f"<b>{get_translation('observation_date', lang=lang).capitalize()}</b>: %{{hovertext}}<br>"
+        f"<b>{get_translation('latitude', lang=lang).capitalize()}</b>: %{{lat}}<br>"
+        f"<b>{get_translation('longitude', lang=lang).capitalize()}</b>: %{{lon}}<br>"
+        f"<b>{get_translation('nb_species', lang=lang).capitalize()}</b>: %{{customdata[0]}}<br>"
+        f"<b>{get_translation('VDL_long', lang=lang)}</b>: %{{customdata[1]:.1f}}<br>"
+        f"<b>{get_translation('deg_toxitolerance', lang=lang).capitalize()}</b>: %{{customdata[2]:.1%}}<br>"
+        f"<b>{get_translation('deg_eutrophication', lang=lang).capitalize()}</b>: %{{customdata[3]:.1%}}<br>"
+        f"<b>{get_translation('deg_acidity', lang=lang).capitalize()}</b>: %{{customdata[4]:.1%}}<br>"
+        "<extra></extra>"
     )
 
     if observation_clicked is not None:
@@ -68,7 +66,7 @@ def create_map_observations(filtered_df, map_column_selected, zoom, center, map_
                     color=observation_clicked_color,
                 ),
                 hoverinfo='skip',  # Hide the hover info, to use the one from the main trace
-                showlegend=False
+                showlegend=False,
             )
         )
 
@@ -88,19 +86,18 @@ def create_map_observations(filtered_df, map_column_selected, zoom, center, map_
 
     return fig_map
 
-def create_map_species_present(filtered_df, map_column_selected, zoom, center, map_style):
+def create_map_species_present(filtered_df, map_column_selected, zoom, center, map_style, lang):
 
-    filtered_df['date_obs'] = pd.to_datetime(filtered_df['date_obs']).dt.strftime('%d/%m/%Y')
-    filtered_df['selected_species_present_translated'] = filtered_df['selected_species_present'].apply(lambda x: get_translation(str(x)))
+    filtered_df['selected_species_present_translated'] = filtered_df['selected_species_present'].apply(lambda x: get_translation(str(x), lang=lang).capitalize())
 
     fig_map = px.scatter_map(
         filtered_df,
         lat='localisation_lat',
         lon='localisation_long',
         color=map_column_selected,
-        hover_name='date_obs',
+        hover_name='date_obs_formatted',
         labels={ # Rename the columns for the legend
-            'selected_species_present': 'Espèce présente',
+            'selected_species_present': get_translation('selected_species_present', lang=lang).capitalize(),
         },
         custom_data=['selected_species_present_translated'],
         map_style=map_style,
@@ -108,15 +105,16 @@ def create_map_species_present(filtered_df, map_column_selected, zoom, center, m
     )
 
     fig_map.update_traces(
-        hovertemplate='<b>Date d\'observation</b>: %{hovertext}<br>' +
-        '<b>Latitude</b>: %{lat}<br>' +
-        '<b>Longitude</b>: %{lon}<br>' +
-        '<b>Espèce présente</b>: %{customdata[0]}<br>'
+        hovertemplate= f"<b>{get_translation('observation_date', lang=lang).capitalize()}</b>: %{{hovertext}}<br>"
+        f"<b>{get_translation('latitude', lang=lang).capitalize()}</b>: %{{lat}}<br>"
+        f"<b>{get_translation('longitude', lang=lang).capitalize()}</b>: %{{lon}}<br>"
+        f"<b>{get_translation('selected_species_present', lang=lang).capitalize()}</b>: %{{customdata[0]}}<br>"
+        "<extra></extra>"
     )
 
     # Apply the translated names to the legend
     for trace in fig_map.data:
-        trace.name = get_translation(trace.name)
+        trace.name = get_translation(trace.name, lang=lang).capitalize()
 
     fig_map.update_layout(
         PLOTLY_LAYOUT,
@@ -128,15 +126,14 @@ def create_map_species_present(filtered_df, map_column_selected, zoom, center, m
             y=0.02,
             bgcolor='rgba(255, 255, 255, 0.8)',  # Semi-transparent background
             bordercolor='grey',
-            borderwidth=1.5,
+            borderwidth=1,
         ),
     )
 
     return fig_map
 
 
-
-def create_hist1_nb_species(observation_with_vdl_df, nb_species_clicked):
+def create_hist1_nb_species(observation_with_vdl_df, nb_species_clicked, lang):
     hist1 = px.histogram(
         observation_with_vdl_df,
         x='nb_species',
@@ -145,8 +142,8 @@ def create_hist1_nb_species(observation_with_vdl_df, nb_species_clicked):
 
     hist1.update_layout(
         **PLOTLY_LAYOUT,
-        xaxis_title="Nombre d\'espèces",
-        yaxis_title="Nombre de sites",
+        xaxis_title=get_translation('nb_species', lang=lang).capitalize(),
+        yaxis_title=get_translation('nb_observations', lang=lang).capitalize(),
         yaxis_showgrid=True,
         bargap=0.1,
     )
@@ -154,8 +151,8 @@ def create_hist1_nb_species(observation_with_vdl_df, nb_species_clicked):
     # Update hover template
     hist1.update_traces(
         hovertemplate=(
-            "<b>Nombre d'espèces:</b> %{x}<br>"
-            "<b>Nombre de sites:</b> %{y}<br>"
+            f"<b>{get_translation('nb_species', lang=lang).capitalize()}:</b> %{{x}}<br>"
+            f"<b>{get_translation('nb_observations', lang=lang).capitalize()}:</b> %{{y}}<br>"
             "<extra></extra>"
         )
     )
@@ -164,18 +161,18 @@ def create_hist1_nb_species(observation_with_vdl_df, nb_species_clicked):
     if nb_species_clicked:
         hist1.add_shape(
             go.layout.Shape(
-                type="line",
+                type='line',
                 x0=nb_species_clicked, x1=nb_species_clicked,
                 y0=0, y1=1,
                 xref='x', yref='paper',
-                line=dict(color='red', width=2, dash="dot")
+                line=dict(color='red', width=2, dash='dot')
             )
         )
 
     return hist1
 
 
-def create_hist2_vdl(observation_with_vdl_df, vdl_clicked):
+def create_hist2_vdl(observation_with_vdl_df, vdl_clicked, lang):
     hist2 = px.histogram(
         observation_with_vdl_df,
         x='VDL',
@@ -184,8 +181,8 @@ def create_hist2_vdl(observation_with_vdl_df, vdl_clicked):
 
     hist2.update_layout(
         **PLOTLY_LAYOUT,
-        xaxis_title="Valeur de Diversité Lichénique (VDL)",
-        yaxis_title="Nombre de sites",
+        xaxis_title=get_translation('VDL_desc', lang=lang),
+        yaxis_title=get_translation('nb_observations', lang=lang).capitalize(),
         yaxis_showgrid=True,
         bargap=0.1,
     )
@@ -193,8 +190,8 @@ def create_hist2_vdl(observation_with_vdl_df, vdl_clicked):
     # Update hover template
     hist2.update_traces(
         hovertemplate=(
-            "<b>VDL:</b> %{x}<br>"
-            "<b>Nombre de sites:</b> %{y}<br>"
+            f"<b>{get_translation('VDL', lang=lang)}:</b> %{{x}}<br>"
+            f"<b>{get_translation('nb_observations', lang=lang).capitalize()}:</b> %{{y}}<br>"
             "<extra></extra>"
         )
     )
@@ -203,33 +200,33 @@ def create_hist2_vdl(observation_with_vdl_df, vdl_clicked):
     if vdl_clicked:
         hist2.add_shape(
             go.layout.Shape(
-                type="line",
+                type='line',
                 x0=vdl_clicked, x1=vdl_clicked,
                 y0=0, y1=1,
                 xref='x', yref='paper',
-                line=dict(color='red', width=2, dash="dot")
+                line=dict(color='red', width=2, dash='dot')
             )
         )
 
     return hist2
 
 
-def create_hist3(lichen_frequency_df):
+def create_hist3(lichen_frequency_df, lang):
 
     hist3 = px.bar(
         lichen_frequency_df,
-        x="nb_lichen_ratio",
-        y="unique_name",
-        orientation="h",
+        x='nb_lichen_ratio',
+        y='unique_name',
+        orientation='h',
         color_discrete_sequence=BASE_COLOR_PALETTE,
     )
 
     hist3.update_layout(
         **PLOTLY_LAYOUT,
-        xaxis_title="% de quadrats",
-        yaxis_title="",
+        xaxis_title=get_translation('percentage_quadrats', lang=lang).capitalize(),
+        yaxis_title='',
         xaxis_showgrid=True,
-        xaxis_tickformat=".0%",
+        xaxis_tickformat='.0%',
         xaxis=dict(
             range=[0, 1.1],
             dtick=0.25  # Set x-axis ticks every 25%
@@ -240,12 +237,12 @@ def create_hist3(lichen_frequency_df):
     hist3.update_traces(
         hovertemplate=(
             "<b>%{y}</b><br><br>"
-            "<b>Pourcentage:</b> %{x}<br>"
-            "<b>Nombre:</b> %{customdata[0]}<br>"
-            "<b>Nord:</b> %{customdata[1]}<br>"
-            "<b>Sud:</b> %{customdata[2]}<br>"
-            "<b>Ouest:</b> %{customdata[3]}<br>"
-            "<b>Est:</b> %{customdata[4]}<br>"
+            f"<b>{get_translation('percentage', lang=lang).capitalize()}:</b> %{{x}}<br>"
+            f"<b>{get_translation('count', lang=lang).capitalize()}:</b> %{{customdata[0]}}<br>"
+            f"<b>{get_translation('north', lang=lang).capitalize()}:</b> %{{customdata[1]}}<br>"
+            f"<b>{get_translation('south', lang=lang).capitalize()}:</b> %{{customdata[2]}}<br>"
+            f"<b>{get_translation('west', lang=lang).capitalize()}:</b> %{{customdata[3]}}<br>"
+            f"<b>{get_translation('east', lang=lang).capitalize()}:</b> %{{customdata[4]}}<br>"
             "<extra></extra>"
         ),
         customdata=lichen_frequency_df[['nb_lichen','nb_lichen_N', 'nb_lichen_S', 'nb_lichen_O', 'nb_lichen_E']].values
@@ -254,7 +251,7 @@ def create_hist3(lichen_frequency_df):
     return hist3
 
 
-def create_pie_thallus(grouped_table_by_observation_and_thallus_df):
+def create_pie_thallus(grouped_table_by_observation_and_thallus_df, lang):
 
     # Capitalize the thallus names
     capital_grouped_table_by_observation_and_thallus_df = grouped_table_by_observation_and_thallus_df.copy()
@@ -262,8 +259,8 @@ def create_pie_thallus(grouped_table_by_observation_and_thallus_df):
 
     pie_thallus = px.pie(
         capital_grouped_table_by_observation_and_thallus_df,
-        names="thallus",
-        values="nb_lichen_id",
+        names='thallus',
+        values='nb_lichen_id',
         color_discrete_sequence=BASE_COLOR_PALETTE[::3],
     )
 
@@ -271,17 +268,18 @@ def create_pie_thallus(grouped_table_by_observation_and_thallus_df):
         **PLOTLY_LAYOUT,
         showlegend=True,
         legend=dict(
-            orientation="h",  # Horizontal legend
-            yanchor="bottom",  # Anchor the legend to the bottom
+            orientation='h',  # Horizontal legend
+            yanchor='top', # Anchor the legend at the top
             y=1.1,  # Position the legend at the top
-            xanchor="center",  # Center the legend horizontally
+            xanchor='center',  # Center the legend horizontally
             x=0.5  # Center the legend horizontally
         ),
     )
 
     # Update hover template and text template
     pie_thallus.update_traces(
-        hovertemplate="<b>%{label}</b><br><b>Nombre:</b> %{value}<extra></extra>",
+        hovertemplate="<b>%{label}</b><br>"
+        f"<b>{get_translation('count', lang=lang).capitalize()}</b>: %{{value}}<extra></extra>",
         texttemplate="%{percent:.0%}"  # Format displayed values as percentages with no decimal places
     )
 
@@ -289,32 +287,32 @@ def create_pie_thallus(grouped_table_by_observation_and_thallus_df):
 
 ## Gauge charts
 
-def create_gauge_chart(value, intervals, color_scale):
+def create_gauge_chart(value, intervals, color_scale, lang):
 
     percentage_value = value * 100
 
     fig = go.Figure(
         go.Indicator(
-            domain={"x": [0, 1], "y": [0, 1]},
+            domain={'x': [0, 1], 'y': [0, 1]},
             value=percentage_value,
-            number={"suffix": "%",
-                    "valueformat": ".0f", # Format the value as a number with no decimals
-                    "font":
-                        {"size": 24 ,
-                         "color": find_color(percentage_value, intervals=intervals, color_scale=color_scale)
+            number={'suffix': '%',
+                    'valueformat': '.0f', # Format the value as a number with no decimals
+                    'font':
+                        {'size': 24 ,
+                         'color': find_color(percentage_value, intervals=intervals, color_scale=color_scale)
                          }
                     },
-            mode="gauge+number",
+            mode='gauge+number',
             gauge={
-                "axis": {"range": [0, 100], "dtick": 25},
-                'bar': {'color': "rgba(60, 60, 60, 1)", 'thickness': 0.4},
-                "steps": [
+                'axis': {'range': [0, 100], 'dtick': 25},
+                'bar': {'color': 'rgba(60, 60, 60, 1)', 'thickness': 0.4},
+                'steps': [
                     {'range': intervals[i:i+2], 'color': color_scale[i]} for i in range(len(intervals) - 1)
                 ],
-                "threshold": {
-                    "line": {"color": "rgba(60, 60, 60, 1)", "width": 3},
-                    "thickness": 0.75,
-                    "value": percentage_value,
+                'threshold': {
+                    'line': {'color': 'rgba(60, 60, 60, 1)', 'width': 3},
+                    'thickness': 0.75,
+                    'value': percentage_value,
                 },
             },
         )
@@ -359,9 +357,9 @@ def create_kpi(value, title=None, intervals=None, color_scale=None):
 
     indicator = go.Indicator(
         value=value,
-        number={"suffix": "%", "font": {"color": color, "size": 50}},
-        mode="number",
-        title={"text": title},
+        number={'suffix': '%', 'font': {'color': color, 'size': 50}},
+        mode='number',
+        title={'text': title},
     )
 
     fig = go.Figure(indicator)
@@ -376,9 +374,9 @@ def create_kpi(value, title=None, intervals=None, color_scale=None):
 
 ## Histograms for species
 
-def create_hist4(count_lichen_per_species_df, user_selection_species_id):
+def create_hist4(count_lichen_per_species_df, user_selection_species_id, lang):
     # Find the index of the selected species ID in the merged table
-    user_selection_idx = count_lichen_per_species_df[count_lichen_per_species_df["species_id"] == user_selection_species_id].index
+    user_selection_idx = count_lichen_per_species_df[count_lichen_per_species_df['species_id'] == user_selection_species_id].index
 
     # Adjust the color of the selected specie to be darker
     pastel_color = PASTEL_COLOR_PALETTE[0]
@@ -389,10 +387,10 @@ def create_hist4(count_lichen_per_species_df, user_selection_species_id):
     # Bar plot
     hist4 = px.bar(
         count_lichen_per_species_df,
-        x="count",
-        y="name",
-        orientation="h",
-        color="name",
+        x='count',
+        y='name',
+        orientation='h',
+        color='name',
         color_discrete_sequence=color_hist4,
         )
 
@@ -404,15 +402,15 @@ def create_hist4(count_lichen_per_species_df, user_selection_species_id):
 
     # Update axes
     hist4.update_xaxes(
-        title_text="Nombre",
+        title_text=get_translation('count', lang=lang).capitalize(),
         showgrid=True,
     )
     hist4.update_yaxes(
-        title="",
-        # tickfont=dict(size=10)  # Adjust tick font size
+        title='',
     )
     hist4.update_traces(
-        hovertemplate="<b>%{y}</b><br><b>Nombre:</b> %{x}<extra></extra>"
+        hovertemplate="<b>%{y}</b><br>"
+        f"<b>{get_translation('count', lang=lang).capitalize()}</b>: %{{x}}<extra></extra>"
     )
 
     return hist4
